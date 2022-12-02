9c4450e94d851927bf013f3f9d8a9dec449720d9
lilia <lilianom59@outlook.fr>
2022-12-02 12:48:15 +0100

    table

--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 let prenoms = ["pierre","paul","jacques"];
-    = "<Ton Prénom>";
+prenoms.splice(0, 1, "Lilian")
 
-document.getElementById('viewport'). = ;
+document.getElementById('viewport').innerHTML = prenoms[0];
 
 
 
